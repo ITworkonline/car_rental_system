052722eb8e7401ac6a933ff5def6ebe37a1f0758
ITworkonline <1130172267@qq.com>
2020-12-03 00:02:00 -0700

    update Mysql

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from config import Config
+from flask_mysqldb import MySQL
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 login = LoginManager(app)
+
 login.login_view = 'login'
 login.login_message = 'You must login to access this page'
 login.login_message_category = 'info'
